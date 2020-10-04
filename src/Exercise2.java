646153773572d34a9cce92d7d85b74fa40c3edde
galaxydancer203 <43752427+galaxydancer203@users.noreply.github.com>
2020-10-04 16:43:35 -0400

    Create Exercise2.java

---
+++ src/Exercise2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Inches : ");
+        int inches = input.nextInt();
+        input.close();
+        int miles = (inches - inches % (12 * 5280)) / (12 * 5280);
+        inches -= miles * 12 * 5280;
+        int feet = (inches - inches % 12) / 12;
+        inches -= feet * 12;
+        System.out.printf("%nMiles  : %,d%nFeet   : %,d%nInches : %,d", miles, feet, inches);
+    }
+}
